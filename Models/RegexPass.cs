1e9b810b6d30a923f23f03218ed8b451c2da8094
Trong Kien Ho <62187768+Kenjilam92@users.noreply.github.com>
2020-06-22 00:33:07 -0400

    fix regex

--- Models/RegexPass.cs
+++ Models/RegexPass.cs
@@ -7,7 +7,7 @@ namespace dojo_activity.Models
     {
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
-            var reg = new Regex(@"^(?=.*?\d)(?=.*?[a-zA-Z])(?=.*?,!@#$%^&*+=)[A-Za-z\d,!@#$%^&*+=]{8,}$");
+            var reg = new Regex(@"^(?=.*?\d)(?=.*?[a-zA-Z])(?=.*?[.!@#$%^&*+=])[A-Za-z\d,!@#$%^&*+=]{8,}$");
             if (!reg.IsMatch((string)value))
                 return new ValidationResult("Password must have at least 1 letter, 1 number and 1 special character.");
             return ValidationResult.Success;
